86c70ac6a0dd1f3981158239db379fade366aabc
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-03 13:07:49 +0800

    First Commit

--- src/main/kotlin/ActivitiesExercise/Activity01-C.kt
+++ src/main/kotlin/ActivitiesExercise/Activity01-C.kt
@@ -1,4 +1,25 @@
-package ActivitiesExercise
+fun main(){
+    print("----Grocery Inventory System----")
+    println(" ")
+    var item1 : String = "-Shampoo"
+    var item2 : String = "-Soap"
+    var item3 : String = "-Detergent Powder"
+    var item4 : String = "-Pencil"
+    var item5 : String = "-Bondpapers"
+    var item6 : String = "-envelopes"
+    var item7 : String = "-Cavage"
+    var item8 : String = "-Orange"
+    var item9 : String = "-Apple"
 
-class `Activity01-C` {
+    println("TIOLETRIES\n"+item1)
+    println(item2)
+    println(item3)
+    println("SCHOOL SUPPLIES\n"+item4)
+    println(item5)
+    println(item6)
+    println("FRUITS & VEGETABLES\n"+item7)
+    println(item8)
+    println(item9)
+
+
 }
