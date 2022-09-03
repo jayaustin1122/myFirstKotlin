0c8b858d3cc4857d5f5557d697ce70a48acad4a2
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-03 13:54:23 +0800

    Second Commit

--- src/main/kotlin/ActivitiesExercise/Activity01-A.kt
+++ src/main/kotlin/ActivitiesExercise/Activity01-A.kt
@@ -1,4 +1,18 @@
 fun main(){
-    print("")
-    var students : String = "Jay"
+    println("-----------STUDENTS IN MD1P-----------")
+    var student1 : String = "Coco"
+    var student2 : String = "Martin"
+    var student3 : String = "Cardo"
+    println(student1)
+    println(student2)
+    println(student3)
+    println("----------PROGRESS TAKEN----------")
+    println("[1]Incomplete[2]Completing[3]Complete")
+    var pro1 : String ="[1]Incomplete"
+    var pro2 : String ="[2]Completing"
+    var pro3 : String ="[3]Complete"
+    println(student1+" \n\t-is "+pro1+" his Project")
+    println(student2+" \n\t-is "+pro2+" his Project")
+    println(student3+" \n\t-is "+pro3+" his Project")
+
 }
