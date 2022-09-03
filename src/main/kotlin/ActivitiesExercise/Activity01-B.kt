3745345e5fd786c21badd09dec826f753df1d644
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-03 13:06:37 +0800

    First Commit

--- src/main/kotlin/ActivitiesExercise/Activity01-B.kt
+++ src/main/kotlin/ActivitiesExercise/Activity01-B.kt
@@ -1,4 +1,18 @@
-package ActivitiesExercise
+fun main (){
 
-class `Activity01-B` {
+    println("\t==Welcome To Our Library===")
+    println("")
+    println("\t\tBooks You Can Borrow: ")
+    var book1 : String = "Algebra"
+    var book2 : String = "Mathematics"
+    var book3 : String = "Science"
+    var book4 : String = "Comics"
+    var book5 : String = "Newspaper"
+    println("-"+book1)
+    println("-"+book2)
+    println("-"+book3)
+    println("-"+book4)
+    println("-"+book5)
+
+
 }
