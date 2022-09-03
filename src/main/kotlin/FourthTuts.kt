9aba1ca6898cedf317288f627b0db014df22db5b
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-03 15:13:37 +0800

    First Commit

--- src/main/kotlin/FourthTuts.kt
+++ src/main/kotlin/FourthTuts.kt
@@ -1,2 +1,13 @@
-class FourthTuts {
+fun main() {
+    //Enter the User
+    print("Enter Your Grade: ")
+    var grade: Int = readln().toInt()
+    //Checking passed or fail.
+    if (grade >= 75){
+        println("You Passed!")
+    }
+    else {
+        println("You Failed!")
+    }
+
 }
