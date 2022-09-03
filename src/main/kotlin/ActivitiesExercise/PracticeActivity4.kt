6c43e0981a9f11e2c347137da8ff8a5e0d5c7810
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-03 17:23:40 +0800

    First Commit

--- src/main/kotlin/ActivitiesExercise/PracticeActivity4.kt
+++ src/main/kotlin/ActivitiesExercise/PracticeActivity4.kt
@@ -1,12 +1,44 @@
 fun main (){
 
-    print("Enter Grade in Math: ")
-    var math: Double = readln().toDouble()
-    print("Enter Grade in English: ")
-    var english: Double = readln().toDouble()
-    print("Enter Grade in Filipino: ")
-    var filipino: Double = readln().toDouble()
-    var sum : Double = math + english + filipino
-    var result : Double = sum / 3
-    print("Average Grade is "+ result)
+    print ("Enter Product: ")
+    var product : String = readln().toString()
+    print ("Enter Price: ")
+    var price : Double = readln().toDouble()
+    if ((price >= 0) && (price <=10000)) {
+        var discount = ("Price of "+price * .0).toInt()
+        println("Discount is " + discount)
+        var sum: Double =  price - discount.toDouble()
+        var result : Double  = price - discount.toDouble()
+        println("Net Price is : " + result)
+    }else if((price >= 10000) && (price <=20000)) {
+        println("Price of "+product + " is " + price)
+        var discount = (price * .05).toInt()
+        println("Discount is " + discount)
+        var sum: Double =  price - discount.toDouble()
+        var result : Double  = price - discount.toDouble()
+        println("Net Price is : " + result)
+    }else if((price >=20000) && (price <=50000)) {
+        println("Price of "+product + " is " + price)
+        var discount = (price * 0.10).toInt()
+        println("Discount is " + discount)
+        var sum: Double =  price - discount.toDouble()
+        var result : Double  = price - discount.toDouble()
+        println("Net Price is : " + result)
+    }else if((price >=50000) && (price <=100000)) {
+        println("Price of "+product + " is " + price)
+        var discount = (price * 0.15).toInt()
+        println("Discount is " + discount)
+        var sum: Double =  price - discount.toDouble()
+        var result : Double  = price - discount.toDouble()
+        println("Net Price is : " + result)
+    }else if(price > 100000) {
+        println("Price of "+product + " is " + price)
+        var discount = (price * 0.20).toInt()
+        println("Discount is " + discount)
+        var sum: Double =  price - discount.toDouble()
+        var result : Double  = price - discount.toDouble()
+        println("Net Price is : " + result)
+    }else
+        print("Invalid!")
+
 }
