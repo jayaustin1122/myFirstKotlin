828925db1526d536600a236670f545996970c48d
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-03 12:12:19 +0800

    Second Tutorial

--- src/main/kotlin/ActivitiesExercise/PracticeActivity1.kt
+++ src/main/kotlin/ActivitiesExercise/PracticeActivity1.kt
@@ -1,2 +1,11 @@
-package ActivitiesExercise
+fun main() {
+    print("Enter Your First Name: ")
+    var firstName : String = readln()
+    print("Enter Your Middle Name/Initial: ")
+    var middleName : String = readln()
+    print("Enter Your Last Name: ")
+    var lastName : String = readln()
 
+    println("Your Name is $firstName $middleName $lastName")
+
+}
