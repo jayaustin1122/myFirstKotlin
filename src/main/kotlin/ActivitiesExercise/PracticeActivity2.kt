7a46dd8f2d433d9e471392bf15595f1560d6e21a
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-03 14:55:59 +0800

    Second Commit

--- src/main/kotlin/ActivitiesExercise/PracticeActivity2.kt
+++ src/main/kotlin/ActivitiesExercise/PracticeActivity2.kt
@@ -1,4 +1,12 @@
-package ActivitiesExercise
+fun main (){
 
-class PracticeActivity2 {
+    print("Enter Grade in Math: ")
+    var math: Double = readln().toDouble()
+    print("Enter Grade in English: ")
+    var english: Double = readln().toDouble()
+    print("Enter Grade in Filipino: ")
+    var filipino: Double = readln().toDouble()
+    var sum : Double = math + english + filipino
+    var result : Double = sum / 3
+    print("Average Grade is "+ result)
 }
---
+++ src/main/kotlin/FourthTuts.kt
@@ -0,0 +1,2 @@
+class FourthTuts {
+}
