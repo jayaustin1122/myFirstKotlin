7150866d667772eb72a3783d852578bb945fbde8
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-05 19:55:52 +0800

    First Commit

--- src/main/kotlin/SevenTuts.kt
+++ src/main/kotlin/SevenTuts.kt
@@ -1,2 +1,17 @@
-class SevenTuts {
+fun main (){
+
+// while loops
+//    var counter = 1
+
+//    while (counter<=5){
+//        println("Helloworld")
+//        counter++
+
+        //do while loop
+        var counter  = 1
+    do {
+        println("hahaha")
+        counter++//execution
+    }while (counter <= 5)//condition
+
 }
