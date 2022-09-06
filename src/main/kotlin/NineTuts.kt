9b3ca4ee16feee5ebc404651e7ab1ead89571c25
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-06 19:35:05 +0800

    First Commit

---
+++ src/main/kotlin/NineTuts.kt
@@ -0,0 +1,15 @@
+fun main (){
+
+    var myString : String? = "HelloWorld"
+    //?.let{}
+//    var count = myString?.let { 2+2 }
+//    println(count)
+
+    // elvis operator?:
+//    val count2 = myString?.length ?: -1
+//    println(count2)
+    //!!
+//    val count3 = myString!!.length
+//    println(count3)
+
+}
