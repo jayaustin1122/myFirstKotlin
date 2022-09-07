85a7aaf1d69f3e051113fe54f2ccafd05b5eaa79
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-07 20:22:19 +0800

    First Commit

--- src/main/kotlin/ElevenTuts.kt
+++ src/main/kotlin/ElevenTuts.kt
@@ -1,2 +1,15 @@
-class ElevenTuts {
+fun main(){
+    var names = arrayOf("john","Paul","Mark","Ringo","George")
+    var counter = 0
+    while (counter < names.size) {
+        println(names[counter])
+        counter++
+    }
+    //display new array
+    var counter1 : Int = 0
+    while (counter1 < names.size){
+        println(names[counter1])
+
+        counter1++
+    }
 }
