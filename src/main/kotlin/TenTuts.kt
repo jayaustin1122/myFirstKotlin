e36b043fd4a41949cf63ea51be47a9ab9c9f72be
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-07 19:55:18 +0800

    First Commit

---
+++ src/main/kotlin/TenTuts.kt
@@ -0,0 +1,14 @@
+fun main (){
+
+    //implicit declaration
+    // array size
+    var names = arrayOf("pogi","mingming","toto")
+    println(names)
+
+    //explicit
+    var number = arrayOf(1,2,3.4,5)
+    println(number[2])
+
+    //array size
+    println(number.size)
+}
