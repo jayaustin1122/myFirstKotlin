6f3f3ea73c825253056abd634c59251f0412cc60
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-07 20:04:23 +0800

    Second Commit

--- src/main/kotlin/TenTuts.kt
+++ src/main/kotlin/TenTuts.kt
@@ -2,13 +2,18 @@ fun main (){
 
     //implicit declaration
     // array size
-    var names = arrayOf("pogi","mingming","toto")
-    println(names)
+    var names = arrayOf("john","paul","mark")
+//    println(names)
 
     //explicit
     var number = arrayOf(1,2,3.4,5)
-    println(number[2])
+//    println(number[2])
 
     //array size
-    println(number.size)
+//    println(number.size)
+
+    //assigning new value
+    names[0] = "Karl"
+
+
 }
