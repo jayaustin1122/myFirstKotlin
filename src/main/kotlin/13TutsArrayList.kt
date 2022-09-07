61a4fc9891938ee707274253d4b9142c20aabe10
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-07 20:50:50 +0800

    First Commit

--- src/main/kotlin/13TutsArrayList.kt
+++ src/main/kotlin/13TutsArrayList.kt
@@ -1,12 +1,26 @@
 fun main (){
 
     var names = ArrayList<String>()
-    names.add("John")
-    names.add("Peter")
+//    names.add("John")
+//    names.add("Peter")
 //ADD elements to a list
-    println(names[0])
-    println(names[1])
+//    println(names[0])
+//    println(names[1])
 //REMOVE elements to a list
-    names.remove(names[0])
-    println(names[0])
+//    names.remove(names[0])
+//    println(names[0])
+
+// add names to arralist
+    names.add("Mary")
+    names.add("Jenny")
+    names.add("Mark")
+    names.add("Paul")
+    names.add("Bryan")
+    names.add("Roel")
+
+    // Traverse
+    for (value in names){
+        println(" $value ")
+    }
+
 }
