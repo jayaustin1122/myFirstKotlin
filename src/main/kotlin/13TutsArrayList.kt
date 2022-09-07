683ea263c3f244c00dfa47412a9bb7d5a8a1dab7
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-07 20:44:40 +0800

    First Commit

---
+++ src/main/kotlin/13TutsArrayList.kt
@@ -0,0 +1,12 @@
+fun main (){
+
+    var names = ArrayList<String>()
+    names.add("John")
+    names.add("Peter")
+//ADD elements to a list
+    println(names[0])
+    println(names[1])
+//REMOVE elements to a list
+    names.remove(names[0])
+    println(names[0])
+}
