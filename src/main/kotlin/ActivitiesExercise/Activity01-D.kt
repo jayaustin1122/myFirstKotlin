ab57de339c8ac30e01304e5be10a924cd6af9efc
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-08 20:55:49 +0800

    Third Commit

--- src/main/kotlin/ActivitiesExercise/Activity01-D.kt
+++ src/main/kotlin/ActivitiesExercise/Activity01-D.kt
@@ -1,28 +1,34 @@
 
 fun main (){
-    print("Enter Any Amount: ")
-    var i: String = readln().toString()
-    var result = 0
-    while (true) {
-        //enter the user
-
-        print("Enter Again?Yes?No: ")
-        var ask: String = readln().toString()
-        // Checking
-        if (ask == "No" && ask == "no") {
-            break
-        }else if (ask == "Yes" && ask == "yes"){
-            result += i.toInt()
+    var counter: Double = 0.toDouble()
+    while (true){
+//Input users amount
+        print("Enter any Amount: ")
+        var amount = readln().toDouble()
+//Ask the user to enter again.
+        print("Enter Again?Yes/No: ")
+        var select = readln().toString()
+        counter+=amount
+//checking input of the user into Conditions
+        if (select == "Yes" || select == "yes") {
             continue
-        }else
-            println("Total of amounts is $result")
+        }else if (select == "No" || select == "no") {
+
+            println("Total of amounts is $counter")
             print("Divide the value by how many?: ")
-        // Division
-            var enterNum: Int = readln().toInt()
-            var finalResult = result/enterNum
+            // Division
+            var enterNum = readln().toDouble()
+            var finalResult = counter / enterNum
             println("Equals $finalResult")
+            println("Thankyou!")
+            break
+
+        }else println("Invalid Input!\nExiting..")
             break
     }
-    print("Thank You!")
+
 }
+
+
+
 
