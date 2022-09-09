405b137523f2caa88c9d23d7cc6dbff5fe9ba85e
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-09 21:03:41 +0800

    First Commit

--- src/main/kotlin/ActivitiesExercise/mynotes.kt
+++ src/main/kotlin/ActivitiesExercise/mynotes.kt
@@ -1,4 +1,18 @@
-package ActivitiesExercise
+fun main (){
+    var counter = 0
+    var numbers = arrayListOf<Int>()
+    for  (i in 0..5) {
+        //input user
+        print("Enter Number: ")
+        var input = readln().toInt()
+        counter ++
+        numbers.add(input)
+        //stop loop when 5
+        if (counter == 5){
+            for (j in numbers)
+            println(j)
+            break
+        }
+    }
 
-class mynotes {
 }
