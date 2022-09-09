a5e078ce4990d1bf5c03041ae41663ef945a6383
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-09 20:28:02 +0800

    First Commit

--- src/main/kotlin/16Map.kt
+++ src/main/kotlin/16Map.kt
@@ -1,2 +1,26 @@
-class `16Map` {
+fun main(){
+    //Map = Keys and value / Immutable
+    var map = mapOf(1 to "Peter", 2 to "Paul", 3 to "Mary")
+    //value
+    println(map[1])//peter
+    println(8)//result is null because out of range
+    println(map.get(2))
+    // display keys
+    for (key in map.keys){
+        println(key)
+
+    }
+    //transverse
+    for (key in map.values) {
+        println(key)
+    }
+    //-------------------------------------------------
+    // Mutable map
+    var map2 = mutableMapOf<Int,String>()
+    //add value
+    map2.put(2,"John")
+    println(map2)
+    //remove element
+    map2.remove(2)
+    println(map2)
 }
