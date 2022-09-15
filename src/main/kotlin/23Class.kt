0be05e78e4e6d47c2bfa1a0318b71f7f3b9f9d58
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-15 13:55:13 +0800

    First Commit

--- src/main/kotlin/23Class.kt
+++ src/main/kotlin/23Class.kt
@@ -1,2 +1,28 @@
-class `23Class` {
+fun main(){
+    var joe = Person("Joe",21)
+    println("*************")
+    println("${joe.name}'s current age is ${joe.name}")
+    println("After 1 Year")
+    joe.ageUp()
+    println("${joe.name}'s current age is ${joe.name}")
+
+}
+class Person(var name: String ,var age :Int){
+    init {
+        println("Object Created")
+        println("Name is $name")
+        println("Age is $age")
+    }
+    fun walk(){
+        println("A Person Walks")
+    }
+    fun eat(){
+        println("A Person Eats")
+    }
+    fun displayAge(){
+        println(age)
+    }
+    fun ageUp(){
+        age++
+    }
 }
