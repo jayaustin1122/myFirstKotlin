e433aaefed02f64a191afba5bde06aed2516854c
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-15 13:54:44 +0800

    First Commit

--- src/main/kotlin/22Class.kt
+++ src/main/kotlin/22Class.kt
@@ -1,2 +1,27 @@
-class `22Class` {
+fun main(){
+//    //Objects
+//    var joe = Person()
+//    //re assigning value
+//    joe.name = "Joe"
+//    //calling object
+//    println(joe.name)//joe
+//
+//    var ana = Person()
+//    println(ana.name)
+    var joe = Person("Joe",21)
+
+
+}
+//class
+class Person1(var name:String,var age :Int) {    //constructor
+    //Attributes
+//    var name: String = "John"
+//    var age: Int = 21
+    //Behaviors
+    fun walk(){
+        println("A Person Walks")
+    }
+    fun eat(){
+        println("A Person Eats")
+    }
 }
