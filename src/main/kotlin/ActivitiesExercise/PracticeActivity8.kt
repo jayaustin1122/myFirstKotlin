9aa88c9f59d7ac3b04d7d5017fb6fdf461075624
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-15 13:51:40 +0800

    First Commit

--- src/main/kotlin/ActivitiesExercise/PracticeActivity8.kt
+++ src/main/kotlin/ActivitiesExercise/PracticeActivity8.kt
@@ -1,4 +1,19 @@
 package ActivitiesExercise
+fun main(){
+    //calling the function
+    println(maxNumber(1,10,3))
 
-class PracticeActivity8 {
+}
+//Function for finding max number
+fun maxNumber(num1:Int,num2:Int,num3:Int):Int {
+    var result = 0
+    //Checking the numbers who are the bigger value.
+    if (num1 > num2 && num1 > num3) {
+        result = num1
+    }else if (num2 > num1 && num2 > num3){
+        result = num2
+    }else if (num3 > num1 && num3>num2) {
+        result = num3
+    }
+    return result
 }
