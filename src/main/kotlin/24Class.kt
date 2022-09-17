39a8aca9913a89a97a16bcdc069cb40b11de40b1
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-17 20:06:17 +0800

    First Commit

--- src/main/kotlin/24Class.kt
+++ src/main/kotlin/24Class.kt
@@ -1,2 +1,16 @@
-class `24Class` {
+fun  main (){
+
+
+
+}
+class Car(var modelName : String , var fuelVulome : Int){
+    init {
+        println("Object Created")
+        println("ModelName is $modelName")
+        println("fuelVulome is $fuelVulome")
+    }
+    fun accelerate(fuelVulome: Int){
+
+    }
+
 }
