355269d9bc51caf23783a54c354497d4d15a6997
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-17 20:04:27 +0800

    First Commit

--- src/main/kotlin/23Class.kt
+++ src/main/kotlin/23Class.kt
@@ -1,18 +1,26 @@
 fun main(){
+    //Calling primary see the parameters to identify the first or
+    // second constructor is used.
     var joe = Person("Joe",21)
     println("*************")
     println("${joe.name}'s current age is ${joe.name}")
     println("After 1 Year")
     joe.ageUp()
     println("${joe.name}'s current age is ${joe.name}")
-
+    //calling second constructor
+    var anne = Person("Anne")
+    anne.age = 25
+    anne.displayAge()
 }
-class Person(var name: String ,var age :Int){
+class Person(var name: String ,var age :Int){//Primary Constructor
     init {
         println("Object Created")
         println("Name is $name")
         println("Age is $age")
     }
+    //Secondary Constructor
+    constructor(name:String):this(name,0){}
+    constructor(age:Int):this("default_name",age){}
     fun walk(){
         println("A Person Walks")
     }
@@ -20,6 +28,7 @@ class Person(var name: String ,var age :Int){
         println("A Person Eats")
     }
     fun displayAge(){
+        //var age = 7
         println(age)
     }
     fun ageUp(){
---
+++ src/main/kotlin/ActivitiesExercise/PracticeActivity10.kt
@@ -0,0 +1,4 @@
+package ActivitiesExercise
+
+class PracticeActivity10 {
+}
