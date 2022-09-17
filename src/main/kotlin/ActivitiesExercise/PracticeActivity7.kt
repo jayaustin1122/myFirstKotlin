636ef0a7e5b3baf60674e3d45af9ea046385f865
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-17 20:08:13 +0800

    First Commit

--- src/main/kotlin/ActivitiesExercise/PracticeActivity7.kt
+++ src/main/kotlin/ActivitiesExercise/PracticeActivity7.kt
@@ -1,4 +1,22 @@
-package ActivitiesExercise
+fun main (){
 
-class PracticeActivity7 {
+    print("Enter length: ")
+    var num1 = readln().toDouble()
+    CalculatePerimeter(num1.toDouble())
+    print("Enter width: ")
+    var num2 = readln().toDouble()
+    CalculateArea(num2.toDouble())
+    var area = num1 * num2
+    var periMeter = 2*(num1 + num2)
+    println("Perimeter is $periMeter")
+    println("Area is is $area meters")
+}
+fun CalculatePerimeter( num1 : Double): Double {
+    var lenght = num1
+    return lenght
+}
+fun CalculateArea(num2 : Double):Double{
+    var width = num2
+    return width
+
 }
