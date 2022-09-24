de1a72ef513c0b6a6124b9789fbbe9bcb00cc566
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-24 10:35:51 +0800

    First Commit

--- src/main/kotlin/ActivitiesExercise/PracticeActivity8.kt
+++ src/main/kotlin/ActivitiesExercise/PracticeActivity8.kt
@@ -1,19 +1,23 @@
 package ActivitiesExercise
 fun main(){
     //calling the function
-    println(maxNumber(1,10,3))
+    var numComparator = NumberComparator()
+    println(numComparator.maxNumber(1,10,3))
 
 }
-//Function for finding max number
-fun maxNumber(num1:Int,num2:Int,num3:Int):Int {
-    var result = 0
-    //Checking the numbers who are the bigger value.
-    if (num1 > num2 && num1 > num3) {
-        result = num1
-    }else if (num2 > num1 && num2 > num3){
-        result = num2
-    }else if (num3 > num1 && num3>num2) {
-        result = num3
+class NumberComparator() {
+    //Function for finding max number
+    fun maxNumber(num1: Int, num2: Int, num3: Int): Int {
+        var result :Int
+        //Checking the numbers who are the bigger value.
+        if (num1 >= num2 && num1 >= num3) {
+            result = num1
+        } else if (num2 >= num1 && num2 >= num3) {
+            result = num2
+        } else if (num3 >= num1 && num3 >= num2) {
+            result = num3
+        }else
+            result = 0
+        return result
     }
-    return result
 }
