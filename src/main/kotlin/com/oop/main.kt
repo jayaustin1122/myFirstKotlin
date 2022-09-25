22010f3027b1e7f16881c2ee9f1e9a2357b1afc2
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-25 12:36:47 +0800

    First Commit

--- src/main/kotlin/com/oop/main.kt
+++ src/main/kotlin/com/oop/main.kt
@@ -1,4 +1,14 @@
 package com.oop
-
-class main {
+fun main(){
+    //calling abstraction and encapsulation
+//    var joePerson = Person()
+//    joePerson.name = "Peter"
+//    println(joePerson.name)
+//    joePerson.age = -1
+//    println("HelloWorld")
+    //calling the subclass student
+    var joeStudent = Student(1212)
+    println( joeStudent.name)
+    joeStudent.age = -1
+    joeStudent.displayStudentId()
 }
