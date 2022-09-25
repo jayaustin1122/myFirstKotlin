4af191c77bf8b76742474c6c360c0d63963916e8
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-25 12:33:53 +0800

    Fourth Commit

--- src/main/kotlin/ActivitiesExercise/Activity01-D.kt
+++ src/main/kotlin/ActivitiesExercise/Activity01-D.kt
@@ -2,18 +2,17 @@
 fun main (){
     var counter: Double = 0.toDouble()
     while (true){
-//Input users amount
+        //Input users amount
         print("Enter any Amount: ")
         var amount = readln().toDouble()
-//Ask the user to enter again.
+        //Ask the user to enter again.
         print("Enter Again?Yes/No: ")
         var select = readln().toString()
         counter+=amount
-//checking input of the user into Conditions
+        //checking input of the user into Conditions
         if (select == "Yes" || select == "yes") {
             continue
         }else if (select == "No" || select == "no") {
-
             println("Total of amounts is $counter")
             print("Divide the value by how many?: ")
             // Division
@@ -22,13 +21,8 @@ fun main (){
             println("Equals $finalResult")
             println("Thankyou!")
             break
-
         }else println("Invalid Input!\nExiting..")
             break
     }
 
 }
-
-
-
-
