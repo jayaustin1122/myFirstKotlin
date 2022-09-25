7a49640aec8ad398a9f557d7f05c612568599ddb
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-25 12:36:21 +0800

    First Commit

--- src/main/kotlin/com/oop/Student.kt
+++ src/main/kotlin/com/oop/Student.kt
@@ -1,4 +1,16 @@
 package com.oop
+//inheritance
+class Student(_studentId:Int) : Person() {
+    var studentId = _studentId
 
-class Student {
+    fun displayStudentId(){
+        println(studentId)
+
+    }
+
+    override fun displayName() {
+        super.displayName()// overiding or changing the value of the super class.
+        println("Displayin subclass student $name")
+
+    }
 }
