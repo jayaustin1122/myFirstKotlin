223d23533760fd0bd9bcbd39e4dc3e88c526cba6
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-28 20:36:59 +0800

    First Commit

--- src/test/kotlin/ActivitiesExercise/Activity03_CKtTest.kt
+++ src/test/kotlin/ActivitiesExercise/Activity03_CKtTest.kt
@@ -1,5 +1,36 @@
 package ActivitiesExercise
-
+import MiniLibrary
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class Activity03_CKtTest
+internal class Activity03_CKtTest{
+    /* ---Books---
+     1: Trigo
+     2: Comic Book
+     3: Newspaper
+     4: Science*/
+    //(1) Borrow Trigo Book
+    @Test
+    fun borrow_Trigo(){
+        var library = MiniLibrary()
+        assertEquals(1,library.borrowBooks2(1))
+    }
+    //(2) Borrow Comic Book
+    @Test
+    fun borrow_ComicBook(){
+        var library = MiniLibrary()
+        assertEquals(2,library.borrowBooks2(2))
+    }
+    //(3) Borrow NewsPaper
+    @Test
+    fun borrow_NewsPaper(){
+        var library = MiniLibrary()
+        assertEquals(3,library.borrowBooks2(3))
+    }
+    //(4) Borrow Science
+    @Test
+    fun borrow_Science(){
+        var library = MiniLibrary()
+        assertEquals(4,library.borrowBooks2(4))
+    }
+}
