d258b2e8754afdc183516752115bcab2a53d6a37
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-29 10:30:32 +0800

    First Commit

--- src/test/kotlin/ActivitiesExercise/Activity03_DKtTest.kt
+++ src/test/kotlin/ActivitiesExercise/Activity03_DKtTest.kt
@@ -1,5 +1,29 @@
 package ActivitiesExercise
-
 import org.junit.jupiter.api.Assertions.*
-
-internal class Activity03_DKtTest
+import org.junit.jupiter.api.Test
+internal class Activity03_DKtTest{
+    //add napkin
+    @Test
+    fun checkAddNapkin(){
+        var check = Grocery1()
+        assertEquals("Napkin",check.addItem("Napkin",12.0,3))
+    }
+    //add Tissue
+    @Test
+    fun checkAddTissue(){
+        var check = Grocery1()
+        assertEquals("Tissue",check.addItem("Tissue",10.0,5))
+    }
+    //add Soap
+    @Test
+    fun checkAddSoap(){
+        var check = Grocery1()
+        assertEquals("Soap",check.addItem("Soap",15.0,12))
+    }
+    //add Toothbrush
+    @Test
+    fun checkAddToothBrush(){
+        var check = Grocery1()
+        assertEquals("Toothbrush",check.addItem("Toothbrush",15.0,12))
+    }
+}
