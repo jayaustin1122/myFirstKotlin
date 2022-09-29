0ce6527c503d82482a7c74798a073d6dc6d86484
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-29 12:42:49 +0800

    First Commit

--- src/main/kotlin/ActivitiesExercise/Activity03-B.kt
+++ src/main/kotlin/ActivitiesExercise/Activity03-B.kt
@@ -1,4 +1,40 @@
 package ActivitiesExercise
-
-class `Activity03-B` {
+/*
+Activity 03 - B
+Create JUnit test for Activity 01 - D
+ */
+fun main (){
+    fun newComputeAmount() = Amount()
+    //Input users amount
+    var counter : Double = 0.0
+    while (true){
+        print("Enter any Amount: ")
+        var amount = readln().toDouble()
+        newComputeAmount().computeAmount(amount)
+        print("Enter Again?Yes/No: ")
+        var select = readln()
+        counter+=amount
+        //checking input of the user into Conditions
+        if (select == "Yes" || select == "yes") {
+            continue
+        }else if (select == "No" || select == "no") {
+            println("Total of amounts is $counter")
+            print("Divide the value by how many?: ")
+            // Division
+            var enterNum = readln().toDouble()
+            var finalResult = counter / enterNum
+            println("Equals $finalResult")
+            println("Thankyou!")
+            break
+        }else println("Invalid Input!\nExiting..")
+        break
+    }
+}
+//hf
+class Amount{
+    // total input of user
+    fun computeAmount(counter:Double):Double{
+        println("Your Total Input is: $counter")
+    return counter
+    }
 }
