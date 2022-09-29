041545ec2c20fe0d750f388fc26524f2153e70f5
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-09-29 12:44:40 +0800

    First Commit

--- src/test/kotlin/ActivitiesExercise/Activity03_BKtTest.kt
+++ src/test/kotlin/ActivitiesExercise/Activity03_BKtTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class Activity03_BKtTest{
+    //checking if the user input sure go to the function
     @Test
     fun checkOfComputations(){
         var check = Amount()
